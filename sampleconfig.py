8a32540362fa8b58302088c4c146ac8377ad62b0
oddlyspaced1 <oddlyspaced1@gmail.com>
2022-01-22 09:18:25 +0530

    sampleconfig: Setup Mail related params

--- sampleconfig.py
+++ sampleconfig.py
@@ -4,4 +4,6 @@ class Config :
     DEBUG = True
     QUERY_FILE = "queries"
     SEARCH_RESULT_DURATION = VideoDurationFilter.short
-    SEARCH_QUERY_LIMIT = 10
+    SEARCH_QUERY_LIMIT = 1
+    CLIENT_EMAIL = "randomcat@meowmeow.com" # Mail used to send out email
+    RECEIVER_EMAIL = ["goodhuman@ilovecats.com"] # Mail(s) to which the email would be sent
