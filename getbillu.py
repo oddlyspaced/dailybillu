f457e7305690c0a89dee7fee883a45dc9bc7cdc8
oddlyspaced1 <oddlyspaced1@gmail.com>
2022-01-22 09:08:13 +0530

    getbillu: Setup base class with method to get single video link

--- getbillu.py
+++ getbillu.py
@@ -2,14 +2,40 @@
 # Search constraints :
 # length = < 4 Minutes [ Short ]
 
+from nis import cat
 from youtubesearchpython import *
 import random
 from config import Config
 
-# Read random query from queries file
-query = random.choice(open("queries", "r").readlines())
-print(query)
+class Billu :
+    def __init__(self) :
+        pass
 
-cats = CustomSearch(query, Config.SEARCH_RESULT_DURATION, limit = Config.SEARCH_QUERY_LIMIT)
+    # Check if id is present
+    def already_used(self, id) :
+        f = open("used", "r")
+        all_ids = f.readlines()
+        f.close()
+        return id in all_ids
 
-print(cats.result())
+    def save_billu(self, id) :
+        f = open("used", "w+")
+        f.write(id)
+        f.close()
+
+    def get_single_billu(self) :
+        # Read random query from queries file
+        query = random.choice(open("queries", "r").readlines())
+        # Search for billu videos
+        cats = CustomSearch(query, Config.SEARCH_RESULT_DURATION, limit = 1)
+        # Check if we already used the id
+        print(cats.result()['result'][0]["id"])
+        while self.already_used(cats.result()['result'][0]["id"]) is True:
+            cats.next()
+        
+        # new billu found
+        # save it and return link
+        self.save_billu(cats.result()['result'][0]["id"])
+        return cats.result()['result'][0]["link"]
+
+print(Billu().get_single_billu())
