aa051412e40d300d460f8c910051f35e635c6ce1
oddlyspaced1 <oddlyspaced1@gmail.com>
2022-01-22 08:45:28 +0530

    sampleconfig: Setup more configurable parameters

--- sampleconfig.py
+++ sampleconfig.py
@@ -1,5 +1,7 @@
-from pickle import TRUE
-
+from youtubesearchpython import VideoDurationFilter
 
 class Config :
-    DEBUG = TRUE
+    DEBUG = True
+    QUERY_FILE = "queries"
+    SEARCH_RESULT_DURATION = VideoDurationFilter.short
+    SEARCH_QUERY_LIMIT = 10
