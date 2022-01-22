9e2fd375db278e46764a48e6499fe3add6fa3509
oddlyspaced1 <oddlyspaced1@gmail.com>
2022-01-22 09:28:42 +0530

    sampleconfig: Setup Mail related params [2]

--- sampleconfig.py
+++ sampleconfig.py
@@ -6,4 +6,7 @@ class Config :
     SEARCH_RESULT_DURATION = VideoDurationFilter.short
     SEARCH_QUERY_LIMIT = 1
     CLIENT_EMAIL = "randomcat@meowmeow.com" # Mail used to send out email
+    CLIENT_PASSWORD = "meowmeowmeow"
     RECEIVER_EMAIL = ["goodhuman@ilovecats.com"] # Mail(s) to which the email would be sent
+    SMTP_PORT = 587 # 587 is for GMail
+    SMTP_SERVER = "smtp.gmail.com"
