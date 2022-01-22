603873a4359db127188f289e1f49d1457b0a5d98
oddlyspaced1 <oddlyspaced1@gmail.com>
2022-01-22 09:29:29 +0530

    smtpkitty: Use config instead of hardcoded values

--- smtpkitty.py
+++ smtpkitty.py
@@ -1,17 +1,14 @@
 import smtplib, ssl
+from config import Config
 
-port = 587
-smtp_server = "smtp.gmail.com"
-sender_email = "***"
-receiver_email = "***"
-password = input("Enter password of dailybillu:")
 message = """\
 Subject: Hi there
 
 This message is sent from Python."""
 
 context = ssl.create_default_context()
-with smtplib.SMTP(smtp_server, port) as server:
-    server.starttls(context=context)
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+with smtplib.SMTP(Config.SMTP_SERVER, Config.SMTP_PORT) as server:
+    server.starttls(context = context)
+    server.login(Config.CLIENT_EMAIL, Config.CLIENT_PASSWORD)
+    for receiver in Config.RECEIVER_EMAIL :
+        server.sendmail(Config.CLIENT_EMAIL, receiver, message)
