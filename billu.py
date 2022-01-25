361310cf34ceb22979a567dcda82f03c06805065
oddlyspaced1 <oddlyspaced1@gmail.com>
2022-01-25 08:32:02 +0530

    Merge getbillu class into billu

--- billu.py
+++ billu.py
@@ -1,7 +1,41 @@
 import smtplib, ssl
 from config import Config
-from getbillu import GetBillu
 from os.path import exists
+from youtubesearchpython import *
+import random
+
+# Fetches a random cat video link from youtube
+class GetBillu :
+    def __init__(self) :
+        pass
+
+    # Check if id is present
+    def already_used(self, id) :
+        # TODO : Check if used file exts
+        f = open("used", "r")
+        all_ids = f.readlines()
+        f.close()
+        return id in all_ids
+
+    def save_billu(self, id) :
+        f = open("used", "w+")
+        f.write(id)
+        f.close()
+
+    def get_single_billu(self) :
+        # Read random query from queries file
+        query = random.choice(open("queries", "r").readlines())
+        # Search for billu videos
+        cats = CustomSearch(query, Config.SEARCH_RESULT_DURATION, limit = 1)
+        # Check if we already used the id
+        print(cats.result()['result'][0]["id"])
+        while self.already_used(cats.result()['result'][0]["id"]) is True:
+            cats.next()
+        
+        # new billu found
+        # save it and return link
+        self.save_billu(cats.result()['result'][0]["id"])
+        return cats.result()['result'][0]["link"]
 
 if exists("day") == False :
     day = 1
